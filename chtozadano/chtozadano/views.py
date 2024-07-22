c2e927dae7d43511cdf6bdf7f74ba3ee0b4ba3ef
TabarakoAkula <mishabor7@mail.ru>
2024-07-22 13:11:05 +0300

    Fix null page error | redirect to mainpage

--- chtozadano/chtozadano/views.py
+++ chtozadano/chtozadano/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 
 
 class MainPage(View):
     def get(self, request):
         return render(request, "mainpage.html")
+
+
+class RedirectToMainPage(View):
+    def get(self, request):
+        return redirect("mainpage")
