71b7a619289b6c57ea81974338e45fe2c89924ce
TabarakoAkula <mishabor7@mail.ru>
2024-08-06 22:56:25 +0300

    [FIX] Flashbank

--- chtozadano/static_dev/js/lightbox-plus-jquery.js
+++ chtozadano/static_dev/js/lightbox-plus-jquery.js
@@ -11038,9 +11038,9 @@ return jQuery;
   Lightbox.defaults = {
     albumLabel: 'Image %1 of %2',
     alwaysShowNavOnTouchDevices: false,
-    fadeDuration: 600,
+    fadeDuration: 0,
     fitImagesInViewport: true,
-    imageFadeDuration: 600,
+    imageFadeDuration: 0,
     // maxWidth: 800,
     // maxHeight: 600,
     positionFromTop: 50,
